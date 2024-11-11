bfbf8de8ec8131f885fac5fdc2e24f5b6b9c1a5e
NailaOfficial <your_git_email@users.noreply.github.com>
2024-11-12 00:12:51 +0500

    alt add kiya hai

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -14,7 +14,7 @@ const AboutSection = () => {
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <div className="flex justify-center items-start mt-[-50px]">
-          <Image src="/images/about-image.png" width={240} height={240} className="object-contain border radious" />
+          <Image src="/images/about-image.png" alt="about image" width={240} height={240} className="object-contain border radious" />
         </div>
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
