08943a519cf594c7afc6ff9ec073953824564667
NailaOfficial <your_git_email@users.noreply.github.com>
2024-11-11 23:54:04 +0500

    next.js portfolio complete

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,52 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+
+  const handleTabChange = (id) => {
+    setTab(id);
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <div className="flex justify-center items-start mt-[-50px]">
+          <Image src="/images/about-image.png" width={240} height={240} className="object-contain border radious" />
+        </div>
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am an aspiring full-stack web developer with a strong passion for creating interactive and responsive web applications...
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              Skills
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              Education
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              Certifications
+            </TabButton>
+          </div>
+          <div className="mt-8">
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
